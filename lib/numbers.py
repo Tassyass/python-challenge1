5e416acbb0cd76dae82788dacdc370b5b211b1e1
Tassyass <tasniim@DESKTOP-5THH7BR>
2023-12-08 10:29:23 +0300

    completed

--- lib/numbers.py
+++ lib/numbers.py
@@ -11,9 +11,9 @@ def exactly_two_positive(a, b, c):
     return positive_count == 2
 
 # Examples
-# print(exactly_two_positive(2, 4, -3))    # Output: True
-# print(exactly_two_positive(-4, 6, 8))    # Output: True
-# print(exactly_two_positive(4, -6, 9))    # Output: True
-# print(exactly_two_positive(-4, 6, 0))    # Output: False
-# print(exactly_two_positive(4, 6, 10))    # Output: False
-# print(exactly_two_positive(-14, -3, -4)) # Output: False
+print(exactly_two_positive(2, 4, -3))    # Output: True
+print(exactly_two_positive(-4, 6, 8))    # Output: True
+print(exactly_two_positive(4, -6, 9))    # Output: True
+print(exactly_two_positive(-4, 6, 0))    # Output: False
+print(exactly_two_positive(4, 6, 10))    # Output: False
+print(exactly_two_positive(-14, -3, -4)) # Output: False
