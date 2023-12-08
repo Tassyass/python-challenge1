b96d434398613e54687061716faf05b9e2f5bc7f
Tassyass <tasniim@DESKTOP-5THH7BR>
2023-12-08 10:31:52 +0300

    completed

--- lib/iterate.py
+++ lib/iterate.py
@@ -1,16 +1,16 @@
 def solve(s):
-#     vowels = 'aeiou'
-#     consonant_values = {letter: i + 1 for i, letter in enumerate('abcdefghijklmnopqrstuvwxyz')}
+    vowels = 'aeiou'
+    consonant_values = {letter: i + 1 for i, letter in enumerate('abcdefghijklmnopqrstuvwxyz')}
     
-#     max_consonant_value = 0
-#     current_consonant_value = 0
+    max_consonant_value = 0
+    current_consonant_value = 0
     
-#     for char in s:
-#         if char not in vowels:
-#             current_consonant_value += consonant_values[char]
-#         else:
-#             max_consonant_value = max(max_consonant_value, current_consonant_value)
-#             current_consonant_value = 0
+    for char in s:
+        if char not in vowels:
+            current_consonant_value += consonant_values[char]
+        else:
+            max_consonant_value = max(max_consonant_value, current_consonant_value)
+            current_consonant_value = 0
 
 #     # Check one last time in case the string ends with a consonant substring
 #     max_consonant_value = max(max_consonant_value, current_consonant_value)
